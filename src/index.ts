509d803196ec3bd29d8324987413ed84d212bc1b
As3prit3 <houssamhadhadi@gmail.com>
2026-02-27 17:41:10 +0000

    Hyperswitch working, now upgrading to Postgres

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { bootstrap } from "./ledger";
 import Fastify from "fastify";
 import dotenv from "dotenv";
 import db from "./db";
-import fastifyCors from '@fastify/cors';
 import fastifyStatic from "@fastify/static";
 import path from "path";
 
@@ -27,12 +26,6 @@ dotenv.config();
 
 const app = Fastify({ logger: true });
 
-app.register(fastifyCors, {
-  origin: "http://127.0.0.1:5500",
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type"],
-});
-
 app.register(fastifyStatic, {
   root: path.join(__dirname, "../public"),
 });
@@ -48,7 +41,6 @@ let balances: any;
 async function start() {
   balances = await bootstrap();
 
-  // startPayoutWorker(balances);
   // 1️⃣ Create booking
   app.post("/create-booking", async (req, reply) => {
     try {
@@ -66,17 +58,16 @@ async function start() {
       new Date().toISOString()
     );
 
-    const response = await fetch("https://sandbox.hyperswitch.io/payments", {
+    const response = await fetch("http://localhost:8080/payments", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "api-key": "snd_rnAVo1BZFBD4wrknW57RequJOMvERcDM3UWHYCJjgM5uIbQRzAT8oRRgYG6lAn2Q",
+        "api-key": "snd_sKCelJbWIO9OAzro0QX4khHffncVgGWKCLis5V2wy0iEsDQEJi7d5YqPFI2p7YKd",
       },
       body: JSON.stringify({
-          amount: amount * 100,
+          amount: amount,
           currency: "MAD",
           confirm: false,
-          profile_id: "pro_iSspbTAvC6UYfIogBViN",
           metadata: {
             bookingId: bookingId,
           }
@@ -140,47 +131,6 @@ async function start() {
         error: "Internal Server Error",
       });
     }
-
-
-    //---------------------------------------------------------------------
-
-    // const booking = db.prepare(`
-    //     SELECT * FROM bookings WHERE id = ?
-    // `).get(bookingId) as Booking;
-
-    // 🔵 Step A: Topup wallet
-  //   await Transactions.create({
-  //     amount: booking.amount,
-  //     precision: 1,
-  //     currency: "MAD",
-  //     source: "@World",
-  //     destinations: [
-  //       {
-  //           identifier: "@EscrowPool",
-  //           distribution: "left",
-  //       },
-  //       {
-  //           identifier: "@PlatformFees",
-  //           distribution: "20%",
-  //       }
-  //     ],
-  //     reference: `${bookingId}_EscrowPool_amount`,
-  //     allow_overdraft: true,
-  //     description: `Split the incoming payment for ${bookingId} between EscrowPool and PlatformFees`,
-  //   });
-
-  //   await Transactions.create({
-  //     amount: booking.amount,
-  //     precision: 1,
-  //     currency: "POINTS",
-  //     source: "@TokenPool",
-  //     destination: balances.clientBalanceId,
-  //     reference: `${bookingId}_client_topup`,
-  //     description: `Topup client balance with points for ${bookingId}`,
-  //     allow_overdraft: true,
-  //   });
-
-  //   return { bookingId, status: "CREATED", amount };
   });
 
 
@@ -188,13 +138,13 @@ async function start() {
     const event = req.body as any;
     console.log("========WEB HOOK /webhook/hyperswitch reached: ",event)
     // Only handle success
-    if (event.status === "succeeded") {
-      const bookingId = event.bookingId;
+    if (event.event_type === "payment_succeeded") {
+      const paymentId = event.content.object.payment_id;
 
       const booking = db.prepare(`
-        SELECT * FROM bookings WHERE id = ?
-      `).get(bookingId) as Booking;
-        console.log("BOOKING: ", bookingId)
+        SELECT * FROM bookings WHERE hyperswitch_payment_id = ?
+      `).get(paymentId) as Booking;
+        console.log("BOOKING: ", paymentId)
       if (!booking) {
         return reply.status(404).send({ error: "Booking not found===" });
       }
@@ -214,8 +164,8 @@ async function start() {
             currency: "POINTS",
             source: balances.clientBalanceId,
             destination: "@TokenPool",
-            reference: `${bookingId}_client_payback`,
-            description: `Client payback for ${bookingId}`
+            reference: `${paymentId}_client_payback`,
+            description: `Client payback for ${paymentId}`
           },
           {
             amount: booking.amount * 0.8,
@@ -223,8 +173,8 @@ async function start() {
             currency: "MAD",
             source: "@EscrowPool",
             destination: balances.merchantBalanceId,
-            reference: `${bookingId}_escrow_to_merchant`,
-            description: `Transfer from EscrowPool to Merchant balance for ${bookingId}`
+            reference: `${paymentId}_escrow_to_merchant`,
+            description: `Transfer from EscrowPool to Merchant balance for ${paymentId}`
           }
         ]
       };
@@ -237,12 +187,12 @@ async function start() {
       db.prepare(`
         UPDATE bookings
         SET status = ?, inflight_batch_id = ?, updated_at = ?
-        WHERE id = ?
+        WHERE hyperswitch_payment_id = ?
       `).run(
         "PAYOUT_INFLIGHT",
         batchId,
         new Date().toISOString(),
-        bookingId
+        paymentId
       );
 
       return reply.send({ success: true });
@@ -251,126 +201,51 @@ async function start() {
     return reply.send({ ok: true });
   });
 
+  // app.post("/hook", async (req: any, reply) => {
+  //   const { id, status} = req.body;
+
+  //   const booking = db.prepare(`SELECT * FROM bookings WHERE inflight_batch_id = ?`).get(id) as Booking;
+  //   if (!booking)
+  //     return reply.status(404).send({ error: "Unknown inflight transaction" });
+
+  //   if (booking.status === "PAYOUT_COMMITTED")
+  //     return { success: true, message: "Transaction already committed" };
+
+  //   if (booking.status === "PAYOUT_VOIDED")
+  //     return { success: true, message: "Transaction already voided" };
+
+  //   if (status === "commit"){
+  //     await Transactions.updateStatus(id, { status: "commit" });
+  //     db.prepare(`
+  //       UPDATE bookings
+  //       SET status = ?, updated_at = ?
+  //       WHERE id = ?
+  //     `).run(
+  //       "PAYOUT_COMMITTED",
+  //       new Date().toISOString(),
+  //       booking.id
+  //     );
+  //   } else if (status === "void"){
+  //     await Transactions.updateStatus(id, { status: "void" });
+  //     db.prepare(`
+  //       UPDATE bookings
+  //       SET status = ?, updated_at = ?
+  //       WHERE id = ?
+  //     `).run(
+  //       "PAYOUT_VOIDED",
+  //       new Date().toISOString(),
+  //       booking.id
+  //     );
+  //   } else
+  //     return reply.status(400).send({ error: "Invalid hook status" });
 
 
+  //   return { success: true, booking_detail: db.prepare(`
+  //       SELECT * FROM bookings WHERE inflight_batch_id = ?
+  //   `).get(id) as Booking };
+  // });
 
-
-
-
-
-
-
-  //------------------------------------------------------------------
-  // 2️⃣ PSP webhook simulation
-  app.post("/webhook/psp", async (req: any, reply) => {
-    const { bookingId } = req.body;
-
-    const booking = db.prepare(`
-        SELECT * FROM bookings WHERE id = ?
-    `).get(bookingId) as Booking;
-
-    if (!booking) {
-      return reply.status(404).send({ error: "Booking not found" });
-    }
-
-    if (booking.status === "PAYOUT_VOIDED" || booking.status === "PAYOUT_COMMITTED") 
-      return { success: true, message: "Already processed" };
-
-    if (booking.status !== "CREATED")
-      return reply.status(400).send({ error: "Invalid booking status" });
-
-    // Bulk payout transaction to client and merchant
-    const bulkPayOut = {
-        atomic: true,
-        inflight: true,
-        transactions: [
-            {
-                amount: booking.amount,
-                precision: 1,
-                currency: "POINTS",
-                source: balances.clientBalanceId,
-                destination: "@TokenPool",
-                reference: `${bookingId}_client_payback`,
-                description: `Client payback for ${bookingId}`,
-            },
-            {
-                amount: booking.amount * 0.8,
-                precision: 1,
-                currency: "MAD",
-                source: "@EscrowPool",
-                destination: balances.merchantBalanceId,
-                reference: `${bookingId}_escrow_to_merchant`,
-                description: `Transfer from EscrowPool to Merchant balance for ${bookingId}`,
-            },
-        ],
-    };
-    const payOutResponse = await Transactions.createBulk(bulkPayOut);
-    console.log("Bulk transaction response:", payOutResponse);
-
-    const typedResponse = payOutResponse as unknown as BulkInflightResponse;
-    const batchId = typedResponse.data.batch_id;
-    
-    db.prepare(`
-      UPDATE bookings
-      SET status = ?, inflight_batch_id = ?, updated_at = ?
-      WHERE id = ?
-    `).run(
-      "PAYOUT_INFLIGHT",
-      batchId, // VERY IMPORTANT
-      new Date().toISOString(),
-      bookingId
-    );
-
-    return { success: true, booking_detail: db.prepare(`
-        SELECT * FROM bookings WHERE id = ?
-    `).get(bookingId) as Booking };
-  });
-
-  app.post("/hook", async (req: any, reply) => {
-    const { id, status} = req.body;
-
-    const booking = db.prepare(`SELECT * FROM bookings WHERE inflight_batch_id = ?`).get(id) as Booking;
-    if (!booking)
-      return reply.status(404).send({ error: "Unknown inflight transaction" });
-
-    if (booking.status === "PAYOUT_COMMITTED")
-      return { success: true, message: "Transaction already committed" };
-
-    if (booking.status === "PAYOUT_VOIDED")
-      return { success: true, message: "Transaction already voided" };
-
-    if (status === "commit"){
-      await Transactions.updateStatus(id, { status: "commit" });
-      db.prepare(`
-        UPDATE bookings
-        SET status = ?, updated_at = ?
-        WHERE id = ?
-      `).run(
-        "PAYOUT_COMMITTED",
-        new Date().toISOString(),
-        booking.id
-      );
-    } else if (status === "void"){
-      await Transactions.updateStatus(id, { status: "void" });
-      db.prepare(`
-        UPDATE bookings
-        SET status = ?, updated_at = ?
-        WHERE id = ?
-      `).run(
-        "PAYOUT_VOIDED",
-        new Date().toISOString(),
-        booking.id
-      );
-    } else
-      return reply.status(400).send({ error: "Invalid hook status" });
-
-
-    return { success: true, booking_detail: db.prepare(`
-        SELECT * FROM bookings WHERE inflight_batch_id = ?
-    `).get(id) as Booking };
-  });
-
-  app.listen({ port: 3000 }, () => {
+  app.listen({ port: 3000, host: "0.0.0.0" }, () => {
     console.log("🚀 Server running on http://localhost:3000");
   });
 }
